021b598e2afc0cc5bd202abefbe409c860669443
lavenderpurple <122147427+lavenderpurple@users.noreply.github.com>
2024-06-14 13:44:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+fetch(`https://reqres.in/api/users?page=2`)
+.then(res=>res.json())
+.then(result=>{
+    output=''
+    for(i=0;i<result['data'].length;i++)
+        {
+            user=result['data'][i]
+            output+=` <tr>
+                <td>${user.id}</td>
+                <td>${user.email}</td>
+                <td>${user.first_name}</td>
+                <td>${user.last_name}</td>
+                <td><img src="${user.avatar}" alt=""></td>
+            </tr>`
+        }
+    console.table(result['data'])
+    document.getElementById('rec').innerHTML=output
+})
